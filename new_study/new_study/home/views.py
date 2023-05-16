11912a7f9a20469b4a95d4d02ab6a204edf44732
SamirS-li <prof.gramist@gmail.com>
2023-05-16 23:54:49 +0400

    coorected static file;created Menu model;

--- new_study/new_study/home/views.py
+++ new_study/new_study/home/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from home.models import Menu 
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home/index.html')
+    context = {}
+    try:
+        menu = Menu.objects.all()
+    except Exception as err:
+        print(err)
+
+    if menu:
+        context['menu'] = menu
+
+    return render(request,'home/index.html', context)
+
+
+def about(request):
+    return render(request, 'home/index.html')
+
